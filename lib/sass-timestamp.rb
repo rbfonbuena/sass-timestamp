46cb0a28f4fad25e986891058666e62400bfe9b8
tsmsogn <tsmsogn@gmail.com>
2015-02-02 17:36:01 +0900

    add unix timestamp

--- lib/sass-timestamp.rb
+++ lib/sass-timestamp.rb
@@ -6,4 +6,8 @@ module Sass::Script::Functions
     def timestamp()
         return Sass::Script::String.new(Time.now.to_s)
     end
+
+    def unix_timestamp()
+      return Sass::Script::String.new(Time.now.to_i.to_s)
+    end
 end
